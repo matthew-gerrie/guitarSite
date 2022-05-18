7e90e50e78c659aa3a41e3acfa23c28958da3545
matthew-gerrie <103454075+matthew-gerrie@users.noreply.github.com>
2022-05-18 15:49:29 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-/**
- * WEB222 – Assignment 05
- *
- * I declare that this assignment is my own work in accordance with
- * Seneca Academic Policy. No part of this assignment has been
- * copied manually or electronically from any other source
- * (including web sites) or distributed to other students.
- *
- * Please update the following with your information:
- *
- *      Name:       Matthew Gerrie
- *      Student ID: 154422216
- *      Date:       04/05/2022
- */
-
-// All of our data is available on the global `window` object.
 // Create local variables to work with it in this file.
 const { products, categories } = window;
 // For debugging, display all of our data in the console
